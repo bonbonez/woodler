dbab92859c0860ef4b35b530e7781655c2519c2f
Artem Petrov <bonbonez@gmail.com>
2014-05-29 18:31:05 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wood::Application.config.secret_key_base = 'ba6e0dd8d874264f01d373a82f5d2b886d0c06ccd3a3a4eadd6e732adf4441f45de980b6682e36b128c6a2fb632b60825f1098adda88357f50e2af9b4aa8e887'
